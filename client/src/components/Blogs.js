148a4afcfc91d3091fdfd0716a88438e663c3d03
Tristin Greenstein <tristingreenstein@mail.com>
2022-04-11 19:35:18 -0700

    Update Blogs.js

--- client/src/components/Blogs.js
+++ client/src/components/Blogs.js
@@ -8,22 +8,7 @@ const Blogs = () => {
     const [description, setDescription] = useState(false);
     const [subject, setSubject] = useState(false);
     const [tags, setTags] = useState(false);
-    const post = async (e) => {
-        e.preventDefault();
-        const res = await fetch("http://localhost:4000/api/create", {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'credentials': 'include'
-            },
-            body: JSON.stringify({
-                    description: description,
-                    subject: subject,
-                    tags: tags,
-            }),
-            })
-    }
+    
     useEffect(() => {
         const fetchcookie = async () => {
             const res = await fetch("http://localhost:4000/", {
@@ -51,13 +36,8 @@ const Blogs = () => {
         <>
         {authenticated ?
             <div>
-                <h1>Create New Blog</h1>
-                <form onSubmit={post}>
-                    <input type="text" placeholder="Subject"  id="subject" onChange={(e) => {setSubject(e.target.value)}}/>
-                    <input type="text" placeholder="Description"  id="description" onChange={(e) => {setDescription(e.target.value)}}/>
-                    <input type="text" placeholder="Tags"  id="tags" onChange={(e) => {setTags(e.target.value)}}/>
-                    <button type="submit">Submit</button>
-                </form>
+                <h1>Blogs</h1>
+                
             </div>
             :
             <div>You are not logged in</div>
